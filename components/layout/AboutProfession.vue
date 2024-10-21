55dd91029c189aa40b4381c8dc6dd8e14dc69ab2
Julia-Sachkova <julia.rulova0066@gmail.com>
2024-10-21 16:59:25 +0300

    add about prof adaptive

--- components/layout/AboutProfession.vue
+++ components/layout/AboutProfession.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="flex flex-row gap-10">
     <div
-      class="section__card w-3/4 p-10 rounded-3xl flex flex-col justify-between"
+      class="section__card w-3/4 p-10 rounded-3xl flex flex-col justify-between max-[540px]:w-full max-[540px]:p-4 max-[540px]:gap-6"
     >
-      <div class="flex flex-col gap-12">
+      <div class="flex flex-col gap-12 max-[540px]:gap-6">
         <h2 class="section__title">О професии</h2>
 
-        <p class="text-xl text-white tracking-wide">
+        <p class="text-xl text-white tracking-wide max-[540px]:text-base">
           .Net Fullstack разработчик — это универсальный специалист, который
           может создавать сайты и веб-приложения, работая, как над серверной
           частью, то есть бэкендом, так и над визуальной, клиентской частью -
@@ -16,29 +16,93 @@
         </p>
       </div>
 
+      <div>
+        <img
+          src="~/assets/icon/lilRightArr.png"
+          alt="смотреть."
+          class="h-4 -mb-4 arrAnimation"
+        />
+        <ul
+          class="flex-row gap-4 overflow-scroll scrollbar-hide hidden max-[540px]:flex"
+        >
+          <li class="flex flex-col gap-3 py-4 text-white">
+            <span
+              class="section__card-pay-color text-nowrap font-semibold text-[28px]"
+            >
+              от 78 000 руб
+            </span>
+            <span>Junior</span>
+          </li>
+          <li class="flex flex-col gap-2.5 min-w-5 justify-center">
+            <img src="~/assets/icon/star.png" alt="пункт." class="w-3.5" />
+            <img src="~/assets/icon/star.png" alt="пункт." class="w-3.5" />
+            <img src="~/assets/icon/star.png" alt="пункт." class="w-3.5" />
+          </li>
+          <li class="flex flex-col gap-3 py-4 text-white">
+            <span
+              class="section__card-pay-color text-nowrap font-semibold text-[28px]"
+            >
+              от 163 000 руб
+            </span>
+            <span>Middle</span>
+          </li>
+          <li class="flex flex-col gap-2.5 min-w-5 justify-center">
+            <img src="~/assets/icon/star.png" alt="пункт." class="w-3.5" />
+            <img src="~/assets/icon/star.png" alt="пункт." class="w-3.5" />
+            <img src="~/assets/icon/star.png" alt="пункт." class="w-3.5" />
+          </li>
+          <li class="flex flex-col gap-3 py-4 text-white">
+            <span
+              class="section__card-pay-color text-nowrap font-semibold text-[28px]"
+            >
+              от 353 000 руб
+            </span>
+            <span>Lead</span>
+          </li>
+        </ul>
+
+        <span class="text-white opacity-[.6] text-xs">
+          *По данным
+          <a
+            href="https://career.habr.com/salaries?spec_aliases[]=fullstack"
+            class="underline cursor-pointer"
+          >
+            Хабр
+          </a>
+        </span>
+      </div>
+
       <a href="https://t.me/sachkova_mng" class="section__consult-btn">
         Получить консультацию
       </a>
     </div>
 
-    <div class="w-1/4 flex flex-row items-end gap-6 relative">
+    <div
+      class="w-1/4 flex flex-row items-end gap-6 relative max-[540px]:hidden"
+    >
       <ul class="flex flex-col gap-6 w-full">
         <li
           class="section__card p-10 rounded-2xl flex flex-col items-start gap-6 text-white"
         >
-          <span class="section__card-pay">от 78 000 руб</span>
+          <span class="section__card-pay section__card-pay-color"
+            >от 78 000 руб</span
+          >
           <span class="text-xl">Junior</span>
         </li>
         <li
           class="section__card p-10 rounded-2xl flex flex-col items-start gap-6 text-white"
         >
-          <span class="section__card-pay">от 163 000 руб</span>
+          <span class="section__card-pay section__card-pay-color"
+            >от 163 000 руб</span
+          >
           <span class="text-xl">Middle</span>
         </li>
         <li
           class="section__card p-10 rounded-2xl flex flex-col items-start gap-6 text-white"
         >
-          <span class="section__card-pay">от 353 000 руб</span>
+          <span class="section__card-pay section__card-pay-color"
+            >от 353 000 руб</span
+          >
           <span class="text-xl">Lead</span>
         </li>
       </ul>
@@ -70,6 +134,17 @@
   font-weight: 600;
   line-height: 110%;
   white-space: nowrap;
+}
+
+.section__card-pay-color {
+  background: linear-gradient(
+    180deg,
+    #fff 22.5%,
+    rgba(255, 255, 255, 0.7) 100%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 
 .section__title {
@@ -104,5 +179,31 @@
 .section__consult-btn:hover {
   box-shadow: 0px 0px 15.1px 0px #e59cff inset;
   transition: box-shadow 0.2s ease-in-out;
+}
+
+@media screen and (max-width: 540px) {
+  .section__title {
+    font-size: 28px;
+  }
+
+  .section__consult-btn {
+    padding: 16px 24px;
+    border-radius: 12px;
+    font-size: 16px;
+  }
+
+  .arrAnimation {
+    width: max-content;
+    animation: 1s linear 1s infinite alternate slidein;
+  }
+
+  @keyframes slidein {
+    0% {
+      margin-left: 8%;
+    }
+    100% {
+      margin-left: 0%;
+    }
+  }
 }
 </style>
