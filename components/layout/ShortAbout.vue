180f3447ff77f4ee2ecc84cf89873ba5e6637591
Julia-Sachkova <julia.rulova0066@gmail.com>
2024-10-21 14:25:14 +0300

    add short about adaptive

--- components/layout/ShortAbout.vue
+++ components/layout/ShortAbout.vue
@@ -1,13 +1,17 @@
 <template>
-  <ul class="grid grid-cols-3 gap-10">
+  <ul
+    class="grid grid-cols-3 gap-10 max-[540px]:flex max-[540px]:flex-row max-[540px]:gap-3 max-[540px]:overflow-x-scroll max-[540px]:scrollbar-hide listAnimation"
+  >
     <li
       v-for="item in items"
       :key="item.label"
-      class="card p-10 flex flex-col gap-6 items-center justify-center"
+      class="card p-10 flex flex-col gap-6 items-center justify-center max-[540px]:py-3 max-[540px]:px-4 max-[540px]:gap-3 max-[540px]:w-56"
     >
-      <img :src="item.icon" :alt="item.label" />
+      <img :src="item.icon" :alt="item.label" class="max-[540px]:w-10" />
 
-      <span class="text-white text-lg">{{ item.label }}</span>
+      <span class="text-white text-lg text-center max-[540px]:text-base">
+        {{ item.label }}
+      </span>
     </li>
   </ul>
 </template>
@@ -54,5 +58,27 @@ const items = [
   border: 1px solid #e59cff;
   background: rgba(10, 1, 25, 0.6);
   box-shadow: 0px 0px 15.1px 0px rgba(198, 135, 234, 0.5) inset;
+}
+
+@media screen and (max-width: 540px) {
+  .card {
+    border-radius: 12px;
+    box-shadow: 0px 0px 8px 0px rgba(198, 135, 234, 0.5) inset;
+    flex: 0 0 auto;
+  }
+
+  .listAnimation {
+    width: max-content;
+    animation: 12s linear 1s infinite alternate slidein;
+  }
+
+  @keyframes slidein {
+    0% {
+      margin-left: -100%;
+    }
+    100% {
+      margin-left: 5%;
+    }
+  }
 }
 </style>
