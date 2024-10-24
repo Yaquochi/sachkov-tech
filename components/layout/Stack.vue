966192aa9c31a2b1c3cf7d664db68e85574c7a16
Julia-Sachkova <julia.rulova0066@gmail.com>
2024-10-24 16:08:15 +0300

    add consist off + stack adaptive

--- components/layout/Stack.vue
+++ components/layout/Stack.vue
@@ -178,6 +178,16 @@ const infr = [
   }
 }
 
+@media screen and (max-width: 1300px) {
+  .section {
+    gap: 32px;
+  }
+
+  .section__title {
+    font-size: 58px;
+  }
+}
+
 @media screen and (max-width: 540px) {
   .section {
     gap: 24px;
